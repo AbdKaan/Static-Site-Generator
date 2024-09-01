3a3578a3557b1d598300d0467d93f04106c9b2bb
AbdKaan <abdu.kaan@gmail.com>
2024-09-01 16:37:54 +0300

    Added LeafNode

---
+++ src/leafnode.py
@@ -0,0 +1,21 @@
+from htmlnode import HTMLNode
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str = None, value: str = None, props: dict = None):
+        # Value needs to exist
+        if value == None:
+            raise ValueError("There needs to be some value.")
+
+        # Can't have child, children=None
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError()
+        
+        if self.tag == None or self.tag == "":
+            return self.value
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        
---
+++ src/test_leafnode.py
@@ -0,0 +1,24 @@
+import unittest
+
+from leafnode import LeafNode
+
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode(tag="p", value="Test paragraph",  props={"class": "test-class"})
+        assert node.to_html() == '<p class="test-class">Test paragraph</p>'
+
+    def test_no_tag(self):
+        node = LeafNode(tag="", value="You shall not pass!", props={"Why": "??"})
+        assert node.to_html() == "You shall not pass!"
+        
+    def test_value(self):
+        node = LeafNode(tag="p", value="Test paragraph",  props={"class": "test-class"})
+        assert node.value != None
+
+    def test_children(self):
+        node = LeafNode(tag='a', value='somelink')
+        assert node.children == None
+
+if __name__ == "__main__":
+    unittest.main()
